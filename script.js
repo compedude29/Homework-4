040dc08e673c914852471307f6b2826018414702
Jonathan Luis Martinez <jonathan.martinez.305@my.csun.edu>
2019-10-12 23:51:31 -0700

    got 'Start Quiz' button to work

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ var watchEl = document.getElementById("watch");
 var myScoreEl = document.getElementById("myScore");
 
 startButtonEl.addEventListener("click", function(){
+    startButtonEl.innerHTML = "";
     var link = document.createElement('a');
     link.setAttribute("href", "quiz.html");
+    link.innerHTML = "Start Quiz";
+    
     console.log(link);
     startButtonEl.appendChild(link); 
 
